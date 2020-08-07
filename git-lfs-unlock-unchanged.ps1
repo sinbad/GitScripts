ec20aed4cc5c0342d8e6e75be2d0e7545aa672e8
Steve Streeting <steve@stevestreeting.com>
2020-08-07 09:21:23 +0100

    Also don't unlock files which are pending push

--- git-lfs-unlock-unchanged.ps1
+++ git-lfs-unlock-unchanged.ps1
@@ -6,7 +6,9 @@ param (
 
 function Print-Usage {
     Write-Output "Git LFS unlock-unchanged"
-    Write-Output "   Unlock all files which are unchanged locally"
+    Write-Output "   Unlock all files which are unchanged locally."
+    Write-Output "   To be unlocked a file must not be modified in the working"
+    Write-Output "   copy, nor be part of any commits which haven't been pushed."
     Write-Output "Usage:"
     Write-Output "  git-lfs-unlock-unchanged.ps1 [options]"
     Write-Output "Options:"
@@ -37,9 +39,37 @@ foreach ($line in $statusOutput) {
         $modifiedFiles.Add($filename) > $null
     }
 }
+Write-Verbose ("Modified files:`n    " + ($modifiedFiles -join "`n    "))
+
+$lfsPushOutput = git lfs push --dry-run origin
+if (!$?) {
+    Write-Output "ERROR: failed to call 'git lfs push --dry-run'"
+    Exit 5
+}
+
+# Result format is of the form
+# push f4ee401c063058a78842bb3ed98088e983c32aa447f346db54fa76f844a7e85e => Path/To/File
+# With some potential informationals we can ignore
+
+$filesToBePushed = [System.Collections.ArrayList]@()
+foreach ($line in $lfsPushOutput) {
+    if ($line -match "^push ([a-f0-9]+)\s+=>\s+(.+)$") {
+        $oid = $matches[1]
+        $filename = $matches[2]
+        $filesToBePushed.Add($filename) > $null
+    }
+}
+
+# Wrap in @() to avoid collapsing to a single string when only 1 file
+$filesToBePushed = @($filesToBePushed | Select-Object -Unique)
+Write-Verbose ("Files awaiting push: `n    " + ($filesToBePushed -join "`n    "))
 
 # git lfs locks --verify is needed to actually check which ones are ours
 $locksOutput = git lfs locks --verify
+if (!$?) {
+    Write-Output "ERROR: failed to call 'git lfs locks'"
+    Exit 5
+}
 $filesToUnlock = [System.Collections.ArrayList]@()
 foreach ($line in $locksOutput) {
     if ($line -match "^O ([^\t]+)\t+(.+)\s+ID:(\d+).*$") {
@@ -47,9 +77,9 @@ foreach ($line in $locksOutput) {
         $owner = $matches[2]
         $id = $matches[3]
         Write-Verbose "Locked file: $filename"
-        if (-not ($modifiedFiles -contains $filename)) {
+        if (-not ($modifiedFiles -contains $filename) -and -not ($filesToBePushed -contains $filename)) {
             $filesToUnlock.Add($filename) > $null
-            Write-Verbose "  $filename isn't modified, will unlock"
+            Write-Verbose "  $filename isn't modified or awaiting push, will unlock"
         }
     }
 }
