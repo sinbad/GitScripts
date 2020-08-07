bcefd7acb64ef3c21043f8e0c5803375ff573b0b
Steve Streeting <steve@stevestreeting.com>
2020-08-07 10:32:37 +0100

    Added script to fix lockable LFS file read-only attributes

---
+++ git-lfs-fix-attributes.ps1
@@ -0,0 +1,99 @@
+[CmdletBinding()] # Fail on unknown args
+param (
+    [switch]$dryrun = $false,
+    [switch]$help = $false
+)
+
+function Print-Usage {
+    Write-Output "Git LFS Fix Attributes"
+    Write-Output "   Fix the read-only attributes on LFS files which are lockable"
+    Write-Output "   but which are not currently locked. Unlocked files are"
+    Write-Output "   made read-only on checkout but it's possible to accidentally"
+    Write-Output "   have files left read/write when they aren't locked, which"
+    Write-Output "   will only get fixed the next time this file is checked out."
+    Write-Output "Usage:"
+    Write-Output "  git-lfs-fix-attributes.ps1 [options]"
+    Write-Output "Options:"
+    Write-Output " "
+    Write-Output "  -dryrun      : Don't perform actions, just report what would happen"
+    Write-Output "  -verbose     : Print more"
+    Write-Output "  -help        : Print this help"
+
+}
+
+$ErrorActionPreference = "Stop"
+
+if ($help) {
+    Print-Usage
+    Exit 0
+}
+
+Write-Output "Checking file attributes..."
+
+# First get the list of LFS files in the repo (yes, all of them)
+# There is a hacky shortcut I *could* use here which is to compare
+# the result of `git lfs locks --local` and `git lfs locks --verify`
+# git lfs locks --local actually reports read/write files on the assumption 
+# you've locked them, but --verify gives you the real case.
+# However I don't like relying on that undocumented behaviour of --local because 
+# it might change in future (e.g. an actual local DB of locks)
+$allLfsFiles = git lfs ls-files -n
+if (!$?) {
+    Write-Output "ERROR: failed to call 'git lfs ls-files'"
+    Exit 5
+}
+
+# Filter these files to those which are lockable
+$lockableLfsFiles = [System.Collections.ArrayList]@()
+# send files from stdin so we don't have to worry about command line length
+$lockableAttrOut = ($allLfsFiles -join "`n") | git check-attr lockable --stdin
+foreach ($line in $lockableAttrOut) {
+    if ($line -match "^([^:]+):\slockable:\sset$") {
+        $filename = $matches[1]
+        $lockableLfsFiles.Add($filename) > $null
+    }
+}
+Write-Verbose ("Checking attributes on lockable files:`n    " + ($lockableLfsFiles -join "`n    "))
+
+# Now get active locks
+# Use --verify so we know which locks are ours
+$locksOutput = git lfs locks --verify
+if (!$?) {
+    Write-Output "ERROR: failed to call 'git lfs locks'"
+    Exit 5
+}
+$lockedFiles = [System.Collections.ArrayList]@()
+foreach ($line in $locksOutput) {
+    if ($line -match "^O ([^\t]+)\t+(.+)\s+ID:(\d+).*$") {
+        $filename = $matches[1]
+        $owner = $matches[2]
+        $id = $matches[3]
+        $lockedFiles.Add($filename) > $null
+    }
+}
+Write-Verbose ("Currently locked files:`n    " + ($lockedFiles -join "`n    "))
+
+$numFixed = 0
+foreach ($filename in $lockableLfsFiles) {
+    $shouldBeReadOnly = (-not $lockedFiles -contains $filename)
+    $isReadOnly = Get-ItemProperty -Path $filename | Select-Object -Expand IsReadOnly
+    if ($isReadOnly -ne $shouldBeReadOnly) {
+        if ($dryrun) {
+            Write-Verbose "${filename}: read-only should be $shouldBeReadOnly"
+        } else {
+            Write-Verbose "${filename}: setting read-only=$shouldBeReadOnly"
+            Set-ItemProperty -Path $filename -Name IsReadOnly -Value $shouldBeReadOnly
+        }
+        ++$numFixed
+    }
+}
+
+if ($numFixed -gt 0) {
+    if ($dryrun) {
+        Write-Output "Would have fixed $numFixed file attributes."
+    } else {
+        Write-Output "Fixed $numFixed file attributes."
+    }
+} else {
+    Write-Output "All file attributes are OK"
+}
